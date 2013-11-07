9ece812093770b930ae0202ee5f152e3005459a6
lukeschultz <lukeschultz@rgnt2-94-28-dhcp.int.colorado.edu>
2013-11-07 09:16:09 -0700

    ScrabbleQ

---
+++ scrabbleQ/scrabbleQ/MADAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  MADAppDelegate.h
+//  scrabbleQ
+//
+//  Created by Aileen Pierce on 10/21/13.
+//  Copyright (c) 2013 Aileen Pierce. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MADAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
