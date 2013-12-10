cc16a49836c04c575f5dc6c22c985149063bc005
Aileen J Pierce <aileen.pierce@colorado.edu>
2013-12-09 21:12:04 -0700

    grading

--- beatles/beatles/MADAppDelegate.m
+++ beatles/beatles/MADAppDelegate.m
@@ -38,7 +38,7 @@
 {
     // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
     
-    self.viewController.title.text
+    //self.viewController.title.text
 }
 
 - (void)applicationDidBecomeActive:(UIApplication *)application
