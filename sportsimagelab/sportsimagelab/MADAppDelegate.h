a9278040cd2f9f45d7fbb1dd8ffb13b3fa64bbf5
lukeschultz <lukeschultz@rgnt2-93-43-dhcp.int.colorado.edu>
2013-12-14 23:37:58 -0700

    sportslabrevised

---
+++ sportsimagelab/sportsimagelab/MADAppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  MADAppDelegate.h
+//  sportsimagelab
+//
+//  Created by Luke Schultz on 12/14/13.
+//  Copyright (c) 2013 Luke Schultz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class MADViewController;
+
+@interface MADAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) MADViewController *viewController;
+
+@end
---
+++ sportsimagelab/sportsimagelab/MADViewController.h
@@ -0,0 +1,18 @@
+//
+//  MADViewController.h
+//  sportsimagelab
+//
+//  Created by Luke Schultz on 12/14/13.
+//  Copyright (c) 2013 Luke Schultz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MADViewController : UIViewController
+@property (weak, nonatomic) IBOutlet UIImageView *SportImage;
+- (IBAction)buttonPressed:(UIButton *)sender;
+@property (weak, nonatomic) IBOutlet UITextField *nameField;
+@property (weak, nonatomic) IBOutlet UILabel *messageLabel;
+- (IBAction)textFieldDoneEditing:(UITextField *)sender;
+
+@end
